84b186b2bf1ef555527ff664d51f076a57a7b7a3
Gabriel Martins <gabrielcust@gmail.com>
2023-10-09 09:48:20 -0300

    feat: types

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,40 @@
 'use client'
-import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { PessoaFisica, PessoaJuridica, Pessoas } from '../../types'
 
+type apiReturn = {
+  fisicas: PessoaFisica[];
+  juridicas: PessoaJuridica[];
+}
 export default function Home() {
-
+  const [pessoaFisica, setPessoaFisica] = useState<PessoaFisica[]>([]);
+  const [pessoaJuridica, setPessoaJuridica] = useState<PessoaJuridica[]>([]);
   useEffect(() => {
-    fetch('http://localhost:3333/api/pessoa')
-      .then(response => response.json())
-      .then(data => console.log(data))
-
+      const fetchPessoas = async () => {
+        const res = await fetch('http://localhost:3333/api/pessoa')
+        const data: apiReturn = await res.json()
+        console.log(data.juridicas);
+        setPessoaFisica(data.fisicas);
+        setPessoaJuridica(data.juridicas);
+      }
+      fetchPessoas();
   }, [])
 
   return (
     <div>
       <h1>Teste</h1>
+      <ul>
+        {pessoaFisica?.map((pessoa) => (
+          <li key={pessoa.pessoa.id_pessoa}>
+            {pessoa.pessoa.nome}
+          </li>
+        ))}
+        {pessoaJuridica?.map((pessoa) => (
+          <li key={pessoa.id_pessoa_juridica}>
+            {pessoa.razao_social}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
