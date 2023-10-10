cc4845c1b16a69632bab4407fc8aa622ff8b1e66
Gabriel Martins <gabrielcust@gmail.com>
2023-10-10 12:03:13 -0300

    feat: docker compose

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,29 +9,42 @@ type apiReturn = {
 export default function Home() {
   const [pessoaFisica, setPessoaFisica] = useState<PessoaFisica[]>([]);
   const [pessoaJuridica, setPessoaJuridica] = useState<PessoaJuridica[]>([]);
+  const [togglePessoas, setTogglePessoas] = useState(true);
+
   useEffect(() => {
       const fetchPessoas = async () => {
         const res = await fetch('http://localhost:3333/api/pessoa')
         const data: apiReturn = await res.json()
-        console.log(data.juridicas);
-        setPessoaFisica(data.fisicas);
         setPessoaJuridica(data.juridicas);
+        setPessoaFisica(data.fisicas);
       }
       fetchPessoas();
-  }, [])
+    }, [])
+    
+    console.log(pessoaJuridica[0]?.pessoa.nome);
+  const filterByName = (pessoas: (PessoaJuridica | PessoaFisica)[]) => {
+    return pessoas.filter((pessoa) => {
+      return pessoa.pessoa.nome.includes('');
+    })
+  }
 
   return (
     <div>
       <h1>Teste</h1>
+      <div>
+        <button onClick ={ () =>  setTogglePessoas(true) }>Físicas</button>
+        <button onClick ={ () =>  setTogglePessoas(false) }>Jurídicas</button>
+      </div>
       <ul>
-        {pessoaFisica?.map((pessoa) => (
+        {togglePessoas ?
+         filterByName(pessoaFisica)?.map((pessoa) => (
           <li key={pessoa.pessoa.id_pessoa}>
             {pessoa.pessoa.nome}
           </li>
-        ))}
-        {pessoaJuridica?.map((pessoa) => (
+        )):
+        (pessoaJuridica)?.map((pessoa) => (
           <li key={pessoa.id_pessoa_juridica}>
-            {pessoa.razao_social}
+            {pessoa.responsavel}
           </li>
         ))}
       </ul>
